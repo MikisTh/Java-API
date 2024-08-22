1e49628e0a616a85284a30bcf49d46fdf4945d7c
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-22 09:09:03 -0300

    Update HeapSort.java
    Add

--- HeapSort.java
+++ HeapSort.java
@@ -16,3 +16,57 @@ public static void heapSort(int[] array) {
         heapify(array, i, 0);
     }
 }
+public class HeapSort {
+  
+    public void sort(int arr[]) {
+      int n = arr.length;
+  
+      // Build max heap
+      for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+      }
+  
+      // Heap sort
+      for (int i = n - 1; i >= 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+  
+        // Heapify root element
+        heapify(arr, i, 0);
+      }
+    }
+  
+    void heapify(int arr[], int n, int i) {
+      // Find largest among root, left child and right child
+      int largest = i;
+      int l = 2 * i + 1;
+      int r = 2 * i + 2;
+  
+      if (l < n && arr[l] > arr[largest])
+        largest = l;
+  
+      if (r < n && arr[r] > arr[largest])
+        largest = r;
+  
+      // Swap and continue heapifying if root is not largest
+      if (largest != i) {
+        int swap = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = swap;
+  
+        heapify(arr, n, largest);
+      }
+    }
+  
+    // Function to print an array
+    static void printArray(int arr[]) {
+      int n = arr.length;
+      for (int i = 0; i < n; ++i)
+        System.out.print(arr[i] + " ");
+      System.out.println();
+    }
+  
+    // Driver code
+    public static void main(String args[]) {
+      int arr[] = { 1, 12, 9, 5, 6, 10 };
